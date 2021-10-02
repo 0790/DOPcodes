5a2d642709a41d811640e43cfd0e94b4f56ca738
0790 <f20180790@hyderabad.bits-pilani.ac.in>
2021-10-02 15:49:38 +0530

    First

---
+++ SHD/job256_2layer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p gpu_x2
+#SBATCH --nodes=1
+#SBATCH -n 1
+#SBATCH --gres=gpu:1
+#SBATCH --mem 20G
+#SBATCH -t 1-00:00
+#SBATCH --job-name=SNN256_2lay
+#SBATCH -o SNN2layers256run2.out
+#SBACTH -e slurm.%j.err
+#SBATCH --mail-user=f20180790@hyderabad.bits-pilani.ac.in
+#SBATCH --mail-type=ALL
+module load cuda-11.0.2-gcc-10.2.0-3wlbq6u
+srun python3 testff256_2layers.py
---
+++ SHD/job_2layer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p gpu_x2
+#SBATCH --nodes=1
+#SBATCH -n 1
+#SBATCH --gres=gpu:1
+#SBATCH --mem 20G
+#SBATCH -t 1-00:00
+#SBATCH --job-name=SNN2lay
+#SBATCH -o SNN2layers128run2.out
+#SBACTH -e slurm.%j.err
+#SBATCH --mail-user=f20180790@hyderabad.bits-pilani.ac.in
+#SBATCH --mail-type=ALL
+module load cuda-11.0.2-gcc-10.2.0-3wlbq6u
+srun python3 testff128_2layers.py
