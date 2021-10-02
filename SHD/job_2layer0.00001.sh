255a4d25578f779180adfa9e80bf7d2f3e7db3fd
0790 <f20180790@hyderabad.bits-pilani.ac.in>
2021-10-02 22:59:38 +0530

    2 layer lr - 0.00001

---
+++ SHD/job_2layer0.00001.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p gpu_x2
+#SBATCH --nodes=1
+#SBATCH -n 1
+#SBATCH --gres=gpu:1
+#SBATCH --mem 30G
+#SBATCH -t 1-00:00
+#SBATCH --job-name=2_0.00001S
+#SBATCH -o SNN2layers128lr-0.00001/SNN2layers128lr-0.00001run1.out
+#SBACTH -e slurm.%j.err
+#SBATCH --mail-user=f20180790@hyderabad.bits-pilani.ac.in
+#SBATCH --mail-type=ALL
+module load cuda-11.0.2-gcc-10.2.0-3wlbq6u
+srun python3 testff128_2layerslr-0.00001.py
